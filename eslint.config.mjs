5d8f8f1bc962ab8251250ba3ddeafe128bbbaecc
Dan Barr <danbarr@users.noreply.github.com>
2025-09-04 17:36:55 -0400

    Add MCP usage guides (#155)
    * Add custom plugin for MCP metadata
    * Add initial MCP usage guides
    * Add thv install script
    * Add Claude subagent
    * Move Tabs and TabItem to the global scope
    
    ---------
    
    Signed-off-by: Dan Barr <6922515+danbarr@users.noreply.github.com>
    Co-authored-by: Dan Barr <6922515+danbarr@users.noreply.github.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -35,6 +35,9 @@ export default [
         // Add global components from src/theme/MDXComponents.tsx here
         //Columns: 'readonly',
         //Column: 'readonly',
+        Tabs: 'readonly',
+        TabItem: 'readonly',
+        MCPMetadata: 'readonly',
       },
     },
   },
