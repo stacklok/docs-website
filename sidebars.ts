3868ca9604ef0a811b047c8128574939f30b5610
Dan Barr <6922515+danbarr@users.noreply.github.com>
2025-10-02 12:22:51 -0400

    Don't expand the tutorials section by default
    Signed-off-by: Dan Barr <6922515+danbarr@users.noreply.github.com>

--- sidebars.ts
+++ sidebars.ts
@@ -157,7 +157,6 @@ const sidebars: SidebarsConfig = {
         description:
           'Learn how to use ToolHive with these step-by-step tutorials.',
       },
-      collapsed: false,
       items: [
         {
           type: 'link',
